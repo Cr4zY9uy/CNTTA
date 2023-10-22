81b2d85f256855ceeb9a3b2957441375fe80ce18
Cr4zY9uy <heronakamura123@gmail.com>
2023-10-22 11:24:51 +0700

    CNTTA

---
+++ Assets/Scripts/OpenGate.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour
+{
+    public float moveSpeed = 1f; // Tốc độ di chuyển của cửa
+    public float moveDistance = 6f; // Khoảng cách di chuyển của cửa
+
+    private Vector3 originalPosition;
+    private Vector3 targetPosition;
+    private bool isDoorOpen;
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+        targetPosition = originalPosition + Vector3.up * moveDistance;
+        isDoorOpen = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (!isDoorOpen)
+                OpenDoor();
+            else
+                CloseDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        isDoorOpen = true;
+        // Di chuyển cửa lên trên
+        StartCoroutine(MoveDoor(targetPosition));
+    }
+
+    private void CloseDoor()
+    {
+        isDoorOpen = false;
+        // Di chuyển cửa trở lại vị trí ban đầu
+        StartCoroutine(MoveDoor(originalPosition));
+    }
+
+    private System.Collections.IEnumerator MoveDoor(Vector3 target)
+    {
+        float startTime = Time.time;
+        Vector3 startPosition = transform.position;
+        float journeyLength = Vector3.Distance(startPosition, target);
+        
+        while (transform.position != target)
+        {
+            float distCovered = (Time.time - startTime) * moveSpeed;
+            float fractionOfJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(startPosition, target, fractionOfJourney);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/ShipMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMove : MonoBehaviour
+{
+    public float speed = 5f; // Tốc độ di chuyển của nhân vật
+    public float stopTimeInSeconds = 60f; // Thời gian dừng lại (số giây)
+
+    private bool shouldMove = true;
+
+    void Start()
+    {
+        Invoke("StopMovement", stopTimeInSeconds);
+    }
+
+    void Update()
+    {
+        if (shouldMove)
+        {
+            // Di chuyển nhân vật về phía bên phải với góc nghiêng 8 độ
+            float angle = 5f;
+            Vector3 dir = Quaternion.Euler(0, angle, 0) * transform.right;
+            transform.Translate(dir * speed * Time.deltaTime);
+        }
+    }
+
+    void StopMovement()
+    {
+        shouldMove = false; // Dừng di chuyển
+        Debug.Log("Nhân vật đã dừng lại.");
+    }
+}
